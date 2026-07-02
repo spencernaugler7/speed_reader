44d54929d537212a423d2eb0155de6e75ac17724
TheLuckyRogue <spencernaugler7@gmail.com>
2026-07-02 17:22:42 -0400

    finish player skeleton

--- src/views/speed_reader.rs
+++ src/views/speed_reader.rs
@@ -1,31 +1,65 @@
 use dioxus::prelude::*;
 
+// use crate::components::Hero;
+
 const SPEED_READER_CSS: Asset = asset!("/assets/styling/speed-reader.css");
+const PLAY_SVG: Asset = asset!("/assets/player/play.svg");
+const PAUSE_SVG: Asset = asset!("/assets/player/pause.svg");
+const SKIP_NEXT_SVG: Asset = asset!("/assets/player/skip_next.svg");
+const SKIP_PREVIOUS_SVG: Asset = asset!("/assets/player/skip_previous.svg");
 
 #[component]
 pub fn SpeedReader() -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: SPEED_READER_CSS }
         div {
             id: "player",
-            class:"player",
+            // class: "player",
+            document::Link { rel: "stylesheet", href: SPEED_READER_CSS },
             div {
                 id: "reader",
-                class: "reader"
-            }
+                class: "reader",
+                input {
+                    type: "file",
+                    "select pdf"
+                }
+            },
             div {
                 id: "playerControls",
-                class: "player-controls",
-                
+                class: "flex-col",
+
+                input {
+                    class: "player-progress",
+                    type: "range",
+                    disabled: true,
+                    min: 0,
+                    max: 100
+                }
                 div {
-                    id: "progressBar",
-                    class: "progress-bar"
-                },
-                div {
-                    id: "buttonControls",
-                    class: "button-controls"
+                    class: "flex-row",
+                    button {
+                        id: "previous",
+                        class: "player-button",
+                        img {
+                            src: SKIP_PREVIOUS_SVG
+                        }
+                    }
+                    button {
+                        id: "play",
+                        class: "player-button",
+                        img {
+                            src: PAUSE_SVG
+                        }
+                    }
+                    button {
+                        id: "next",
+                        class: "player-button",
+                        img {
+                            src: SKIP_NEXT_SVG
+                        }
+                    }
                 }
             }
         }
+        // Hero {}
     }
 }
